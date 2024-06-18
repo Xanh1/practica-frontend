0376d06c83f6188225a3465d82426b3bd1bdf97d
Miguel Apolo <apolo.miguelx12@gmail.com>
2024-06-17 21:11:41 -0500

    add dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,11 +1,66 @@
+'use client';
+
 import Nav from "@/components/Nav";
+import { useState } from "react";
+import { getProducts } from "@/services/getProduct";
 
 export default function dashboard() {
+
+    const [products, setProducts] = useState([]);
+
+    getProducts().then((data) => {
+        if (data.code == 200) {
+            setProducts(data.context);
+        }
+    });
+
     return (
-        <main>
-            <nav>
-                <Nav />      
+        <main className="w-full h-full flex">
+            <nav className="border">
+                <Nav />
             </nav>
+            <div className="flex-1 border p-4">
+                <header className="flex justify-between">
+                    <h1 className="font-bold text-2xl">Products</h1>
+                    <a className="text-sm font-medium bg-blue-100 p-2 rounded-md" href="">Create product</a>
+                </header>
+                <div className="w-full my-4 flex">
+                    <button className="font-medium text-sm">All products</button>
+                </div>
+                <div>
+                    <table className="w-full">
+                        <thead className="bg-gray-50">
+                            <tr>
+                                <th className="p-3 text-sm font-semibold tracking-wide text-left">Name</th>
+                                <th className="p-3 text-sm font-semibold tracking-wide text-left">Description</th>
+                                <th className="p-3 text-sm font-semibold tracking-wide text-left"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {products.map((product) => {
+                                <tr key={product.uid}>
+                                    <td className="p-3 text-sm text-gray-700 ">
+                                        {product.name}
+                                    </td>
+                                    <td className="p-3 text-sm text-gray-700 ">
+                                        {product.decription}
+                                    </td>
+                                    <td>
+                                        <a href="">
+                                            <svg className="hover:text-yellow-600" class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                                <path fill-rule="evenodd" d="M11.32 6.176H5c-1.105 0-2 .949-2 2.118v10.588C3 20.052 3.895 21 5 21h11c1.105 0 2-.948 2-2.118v-7.75l-3.914 4.144A2.46 2.46 0 0 1 12.81 16l-2.681.568c-1.75.37-3.292-1.263-2.942-3.115l.536-2.839c.097-.512.335-.983.684-1.352l2.914-3.086Z" clip-rule="evenodd" />
+                                                <path fill-rule="evenodd" d="M19.846 4.318a2.148 2.148 0 0 0-.437-.692 2.014 2.014 0 0 0-.654-.463 1.92 1.92 0 0 0-1.544 0 2.014 2.014 0 0 0-.654.463l-.546.578 2.852 3.02.546-.579a2.14 2.14 0 0 0 .437-.692 2.244 2.244 0 0 0 0-1.635ZM17.45 8.721 14.597 5.7 9.82 10.76a.54.54 0 0 0-.137.27l-.536 2.84c-.07.37.239.696.588.622l2.682-.567a.492.492 0 0 0 .255-.145l4.778-5.06Z" clip-rule="evenodd" />
+                                            </svg>
+                                        </a>
+                                    </td>
+                                </tr>
+                            })}
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </main>
     );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,20 +3,20 @@ export default function Nav() {
 
     return (
         <nav className="h-full">
-            <ul>
-                <li className="border">
+            <ul className="flex flex-col jutify-center items-center mx-4">
+                <li className="mt-4 mb-6 hover:scale-110" title="logout">
                     <a href="">
-                        Logout
+                        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15 4H18C19.1046 4 20 4.89543 20 6V18C20 19.1046 19.1046 20 18 20H15M8 8L4 12M4 12L8 16M4 12L16 12" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
                     </a>
                 </li>
-                <li className="border">
+                <li className="my-2 hover:scale-110" title="products">
                     <a href="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 64 64"><g id="Layer_79" data-name="Layer 79"><path d="M58.72,18.2a2,2,0,0,0-1-1.65L33,2.27a2,2,0,0,0-2,0c-.06,0-24.75,14.26-24.8,14.33a2,2,0,0,0-.93,1.68V45.72a2,2,0,0,0,1,1.73L31,61.73a2.06,2.06,0,0,0,2,0L57.73,47.45a2,2,0,0,0,1-1.73C58.72,45.64,58.75,18.27,58.72,18.2ZM32,6.31l20.73,12L45.42,22.5l-20.73-12Zm0,23.94-20.73-12,9.43-5.44,20.72,12ZM9.27,21.74,30,33.71V56.54l-20.73-12ZM34,56.54V33.71l9.42-5.44V35.7a2,2,0,0,0,4,0V26l7.31-4.22V44.57Z"></path></g></svg>
+                        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M3.17004 7.43994L12 12.5499L20.77 7.46994" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M12 21.61V12.54" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M9.93001 2.48004L4.59001 5.44004C3.38001 6.11004 2.39001 7.79004 2.39001 9.17004V14.82C2.39001 16.2 3.38001 17.88 4.59001 18.55L9.93001 21.52C11.07 22.15 12.94 22.15 14.08 21.52L19.42 18.55C20.63 17.88 21.62 16.2 21.62 14.82V9.17004C21.62 7.79004 20.63 6.11004 19.42 5.44004L14.08 2.47004C12.93 1.84004 11.07 1.84004 9.93001 2.48004Z" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
                     </a>
                 </li>
-                <li>
+                <li className="my-2 hover:scale-110" title="batches">
                     <a href="">
-                        
+                        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M23 18C23 18.75 22.79 19.46 22.42 20.06C22.21 20.42 21.94 20.74 21.63 21C20.93 21.63 20.01 22 19 22C17.78 22 16.69 21.45 15.97 20.59C15.95 20.56 15.92 20.54 15.9 20.51C15.78 20.37 15.67 20.22 15.58 20.06C15.21 19.46 15 18.75 15 18C15 16.74 15.58 15.61 16.5 14.88C17.19 14.33 18.06 14 19 14C20 14 20.9 14.36 21.6 14.97C21.72 15.06 21.83 15.17 21.93 15.28C22.59 16 23 16.95 23 18Z" stroke="#000000" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M20.49 17.98H17.51" stroke="#000000" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M19 16.52V19.51" stroke="#000000" strokeWidth="1.5" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M3.17004 7.43994L12 12.5499L20.7701 7.46991" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M12 21.6099V12.5399" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> <path d="M21.61 9.17V14.83C21.61 14.88 21.61 14.92 21.6 14.97C20.9 14.36 20 14 19 14C18.06 14 17.19 14.33 16.5 14.88C15.58 15.61 15 16.74 15 18C15 18.75 15.21 19.46 15.58 20.06C15.67 20.22 15.78 20.37 15.9 20.51L14.07 21.52C12.93 22.16 11.07 22.16 9.93001 21.52L4.59001 18.56C3.38001 17.89 2.39001 16.21 2.39001 14.83V9.17C2.39001 7.79 3.38001 6.11002 4.59001 5.44002L9.93001 2.48C11.07 1.84 12.93 1.84 14.07 2.48L19.41 5.44002C20.62 6.11002 21.61 7.79 21.61 9.17Z" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path> </g></svg>
                     </a>
                 </li>
             </ul>
